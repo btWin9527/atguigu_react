be274a7efbd4a35624743f7a5a077338868721e6
郭吉光 <18434392328@163.com>
2021-02-07 16:31:04 +0800

    react_study: 20210207_git忽略eslint

---
+++ react_staging/19_src_push与replace模式/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from "react";
+import {Route, Switch, Redirect} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About"; // 路由组件
+import Header from "./components/Header"; // 一般组件
+import MyNavLink from "./components/MyNavLink";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <Header/>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <MyNavLink to="/about">About</MyNavLink>
+              <MyNavLink to="/home">Home</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route path="/home" component={Home}/>
+                  <Route path="/about" component={About}/>
+                  {/* redirect路由重定向使用 */}
+                  <Redirect to="/about"/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react_staging/19_src_push与replace模式/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <h2>React Router Demo</h2>
+    );
+  }
+}
+
+export default Header;
