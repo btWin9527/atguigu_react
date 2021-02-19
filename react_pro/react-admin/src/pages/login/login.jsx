107eda59383037403279eddd662ad617e7bbc5af
郭吉光 <18434392328@163.com>
2021-02-19 18:46:36 +0800

    react_study: 新建登录页面

--- react_pro/react-admin/src/pages/login/login.jsx
+++ react_pro/react-admin/src/pages/login/login.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
+import './index.less'
+// const logo = require('./images/logo.png');
+import logo from './images/logo.png';
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        login
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} className="login-logo" alt="logo"/>
+          <h2 className="login-tit">React项目：后台管理系统</h2>
+        </header>
+        <div className="login-form">form</div>
       </div>
     );
   }
